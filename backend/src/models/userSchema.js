cd7f0d00805604fa9c9bcf7431ad022840dd84ae
bbtechpro <billy.barber@gmail.com>
2026-06-01 19:43:00 -0400

    Modified Schema to add a role in the Mong database.

--- backend/src/models/userSchema.js
+++ backend/src/models/userSchema.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
