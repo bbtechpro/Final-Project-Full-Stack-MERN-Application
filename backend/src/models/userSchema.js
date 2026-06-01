81aea8dc1865f54147f9f064f9866e1258f27626
bbtechpro <billy.barber@gmail.com>
2026-06-01 19:25:39 -0400

    Corrected path to AuthMiddleware.

--- backend/src/models/userSchema.js
+++ backend/src/models/userSchema.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const { Schema, model } = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   username: {
     type: String,
     required: true,
