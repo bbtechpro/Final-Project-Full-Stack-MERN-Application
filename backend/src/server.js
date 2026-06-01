730787abe20f4be77afae3e3549426d6c1ee3354
bbtechpro <billy.barber@gmail.com>
2026-06-01 17:15:08 -0400

    Mounted the global error handler middleware at the very bottom.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+const errorHandler = require('./middlewares/errorHandler');
+
+// Enable CORS for all routes (adjust as needed for production)
+app.use(cors());
 
 // Parse global middleware
 app.use(express.json());
@@ -11,5 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 // Mount central routers
 app.use('/api/auth', authRouter); // Accessible via: /api/auth/register and /api/auth/login
 app.use('/api/users', userRouter); // Accessible via: /api/users, /api/users/:id
+
+// Global error handling middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
