88e6cc4a6c5c16afccf45783746177d67514f477
bbtechpro <billy.barber@gmail.com>
2026-06-01 15:28:30 -0400

    Refactored userService so the service layer talks directly to Mongoose (User). It receives raw data arguments, queries MongoDB, runs business rules, and returns JavaScript objects. It does not know Express exists.

---
+++ backend/src/services/userService.js
@@ -0,0 +1,37 @@
+// services/userService.js
+const User = require('../../models/userSchema');
+
+exports.fetchAllUsers = async () => {
+  return await User.find().select('-password');
+};
+
+exports.createNewUser = async ({ username, email, password }) => {
+  // Business/Domain Rule Validation
+  const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+  if (existingUser) {
+    throw new Error('User already exists');
+  }
+
+  const newUser = new User({ username, email, password });
+  await newUser.save();
+
+  const userObj = newUser.toObject();
+  delete userObj.password;
+  return userObj;
+};
+
+exports.fetchUserById = async (id) => {
+  return await User.findById(id).select('-password');
+};
+
+exports.replaceUser = async (id, updates) => {
+  return await User.findByIdAndUpdate(id, updates, { new: true, runValidators: true }).select('-password');
+};
+
+exports.modifyUser = async (id, updates) => {
+  return await User.findByIdAndUpdate(id, updates, { new: true, runValidators: true }).select('-password');
+};
+
+exports.removeUser = async (id) => {
+  return await User.findByIdAndDelete(id);
+};
