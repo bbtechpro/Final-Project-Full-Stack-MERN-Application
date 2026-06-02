6c264440e93a94de91bfa7aefe60a5fefed2d2cd
bbtechpro <billy.barber@gmail.com>
2026-06-02 19:00:31 -0400

    Added title <h1>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        
+      <h1>Welcome to the MERN Project Management App</h1>
+      <p>Use the navigation to explore projects, create new ones, and manage your tasks efficiently.</p>
+
+      </section>
+    </>
+  )
+}
+
+export default App
