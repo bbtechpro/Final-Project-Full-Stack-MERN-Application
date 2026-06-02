eda12d78d0da53b38347e1ea8b531bcbd01999bd
bbtechpro <billy.barber@gmail.com>
2026-06-01 20:29:56 -0400

    Deprecated setting caused the server to not run.

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,8 +11,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
