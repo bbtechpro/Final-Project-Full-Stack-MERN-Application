f140f581ec44d4137be468a693c6c29f4f364975
bbtechpro <billy.barber@gmail.com>
2026-06-01 20:29:15 -0400

    The export.authenticateUser was missing.

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,11 +1,26 @@
 // services/authService.js
 const User = require('../models/userSchema');
-const AppError = require('../utils/AppError'); // 🌟 Import
+const AppError = require('../utils/AppError');
+const bcrypt = require('bcrypt');
+
+exports.authenticateUser = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError('Incorrect email or password.', 400);
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new AppError('Incorrect email or password.', 400);
+  }
+
+  return user;
+};
 
 exports.registerNewUser = async ({ username, email, password }) => {
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    // 🌟 Instantly communicates a 400 Bad Request to the error middleware
+    // Instantly communicates a 400 Bad Request to the error middleware
     throw new AppError('A user with that email already exists.', 400); 
   }
 
