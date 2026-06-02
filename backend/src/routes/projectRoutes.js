06a9284412dec9e93fbf6b73f763fc2600796976
bbtechpro <billy.barber@gmail.com>
2026-06-02 17:09:27 -0400

    Added next() block

---
+++ backend/src/routes/projectRoutes.js
@@ -0,0 +1,23 @@
+// routes/projectRoutes.js
+const express = require('express');
+const router = express.Router();
+const projectController = require('../controllers/projectController');
+const { protect } = require('../middleware/authMiddleware');
+// Validation middleware for project creation and updates
+const validateProjectCreation = (req, res, next) => {
+  const { name, description } = req.validatedBody || {};
+  next();
+  // This function will be implemented in the middleware file
+};  
+
+// Public Endpoints
+router.get('/', projectController.getAllProjects);
+router.get('/:id', projectController.getProjectById);
+
+// Protected Endpoints (Requires Login)
+// Note: We inject the 'protect' middleware to automatically populate req.user
+router.post('/', protect, validateProjectCreation, projectController.createProject);
+router.put('/:id', protect, validateProjectCreation, projectController.updateProject);
+router.delete('/:id', protect, validateProjectCreation, projectController.deleteProject);
+
+module.exports = router;
