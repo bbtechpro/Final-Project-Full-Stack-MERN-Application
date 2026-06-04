8dc37eece746165aaccca40bb4297b937a1b63f7
bbtechpro <billy.barber@gmail.com>
2026-06-04 18:17:46 -0400

    Removed src folder

--- backend/src/routes/loginRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/loginRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Map login endpoint directly to the controller action
-router.post('/', authController.loginUser);
-
-module.exports = router;
--- backend/src/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-// utils/catchAsync.js
-/**
- * Wraps an async function, catching any errors and passing them to next().
- */
-module.exports = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
